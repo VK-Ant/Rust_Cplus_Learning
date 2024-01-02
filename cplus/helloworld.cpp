bc76bac93aca61b9c094adcccf2ef3a6a986052b
Vk <venkatkumar2020ece@gmail.com>
2024-01-02 11:01:01 +0530

    new line \n and endl

--- cplus/helloworld.cpp
+++ cplus/helloworld.cpp
@@ -2,8 +2,12 @@
 
 int main(){
 
-  std::cout << "Hello vk! welcome to C++ journey!" << std::endl;
-  std::cout << "VK performance of 2023 is great!"<< std::endl;
+//endl means initiate the new line
+  //std::cout << "Hello vk! welcome to C++ journey!" << std::endl;
+  //std::cout << "VK performance of 2023 is great!"<< std::endl;
+//the new line also used '\n'
+  std::cout << "Hello vk! welcome to C++ journey!" << '\n';
+  std::cout << "VK performance of 2023 is great!"<< '\n' ;
 
   return 0;
 }

5c0e11c652696390e35fc3018dfc45c4faf682da
Vk <venkatkumar2020ece@gmail.com>
2024-01-02 11:51:15 +0530

    datatype and variable,assignment declaration

---
+++ cplus/variable_declare.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+int main() {
+
+/*
+int x; Declaration
+x = 5; Assignment
+*/
+    //integer (whole number)
+
+    int age = 21;
+    int year = 2023;
+    int days = 7 ;
+
+    //double(number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    //Boolean
+    bool power = true;
+    bool forsale = false;
+
+    //string (objects that represents a sequence of text)
+    std::string name = "VK";
+    std::string day = "Monday";
+    std::string addrees = "123 madurai.";
+
+    std::cout << "Hello "<< name << '\n';
+
+    return 0;
+
+}

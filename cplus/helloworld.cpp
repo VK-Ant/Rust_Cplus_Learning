d709aa23e1248e623cfe72204c85f05eef2084c9
Vk <venkatkumar2020ece@gmail.com>
2024-01-02 10:51:41 +0530

    initial step of 2024

--- cplus/helloworld.cpp
+++ cplus/helloworld.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
 
-int main() {
-  cout << "Hello World!vk";
+int main(){
+
+  std::cout << "Hello vk! welcome to C++ journey!" << std::endl;
+  std::cout << "VK performance of 2023 is great!"<< std::endl;
+
   return 0;
-} 
+}

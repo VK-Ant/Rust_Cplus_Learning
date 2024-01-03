2430c71ce8c131ef30b3d893950ad8be23901ec6
Vk <venkatkumar2020ece@gmail.com>
2024-01-03 12:31:23 +0530

    namespace concept completed

--- cplus/namespace_ns.cpp
+++ cplus/namespace_ns.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 
+/*
+- Name space: provide a solution for preventing name conflicts in large projects. Each entity needs a unique name.
+- A name space allows for identifally named entities as long as the namespaces are different.
+ */
+
 namespace first {
     int x = 1;
 }
@@ -7,6 +12,23 @@ namespace first {
 namespace second {
     int x = 2;
 }
+
+/*
+int main()
+{
+    using namespace std;
+    using std::cout;
+    using std::string; 
+
+    string name = "vk";
+    int x = 10;
+    cout << x << '\n';
+    cout << first::x << '\n';
+    cout << second::x << '\n';
+    return 0;
+
+}
+*/
 /*
 int main()
 {
@@ -22,7 +44,7 @@ int main()
 int main()
 {
     using namespace first;
-    
+
     std::cout << x << '\n';
 
     return 0;

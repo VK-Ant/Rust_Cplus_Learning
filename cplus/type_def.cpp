92f3cf2f2858cc1f6e386d6908f93291ff92e881
Vk <venkatkumar2020ece@gmail.com>
2024-01-04 11:24:27 +0530

    typedef start

---
+++ cplus/type_def.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+
+//Typedef - Reserved keyword used to create an additional name
+//(Alias) for another datatype.
+//New identifier for an existing type.
+//Helps with readability and reduces types
+
+
+return 0;
+
+
+}

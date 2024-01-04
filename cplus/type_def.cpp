c7119929053ec06d5cb93eba4da808e4a4e68eab
Vk <venkatkumar2020ece@gmail.com>
2024-01-04 11:34:09 +0530

    typedef initial

--- cplus/type_def.cpp
+++ cplus/type_def.cpp
@@ -1,13 +1,19 @@
-#include <iostream>
-
-int main()
-{
-
 //Typedef - Reserved keyword used to create an additional name
 //(Alias) for another datatype.
 //New identifier for an existing type.
 //Helps with readability and reduces types
 
+
+#include <iostream>
+#include<vector>
+
+typedef std::vector<std::pair<std::string, int>> pairlist_t;
+
+int main()
+{
+
+
+pairlist_t pairlist;
 
 return 0;
 

7f55f7e946b18b188d492d282fd3afad4a9f02a0
Vk <venkatkumar2020ece@gmail.com>
2024-01-05 11:19:00 +0530

    Arthimetics operation

---
+++ cplus/artithematics.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+int main()
+{
+    //arithemetic operators = Return to the result of specific arithmetic operation (+,/,*,-)
+
+    int students = 28;
+    
+
+    //students = students + 1 ;
+    //students +=1;
+    //students++;
+
+    //students = students - 2;
+    //students -= 1;
+    //students--;
+
+    //students = students * 2;
+    //students *= 2;
+
+    //students = students /2 ;
+    students /= 2;
+
+    std::cout << students << '\n';
+
+    return 0;
+
+}

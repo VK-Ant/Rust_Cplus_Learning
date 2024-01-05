cc74b028c1d1e7da5fe8303afdfc86a0af055af7
Vk <venkatkumar2020ece@gmail.com>
2024-01-05 11:26:33 +0530

    Arthimetics operation all done

--- cplus/artithematics.cpp
+++ cplus/artithematics.cpp
@@ -3,9 +3,14 @@
 int main()
 {
     //arithemetic operators = Return to the result of specific arithmetic operation (+,/,*,-)
+    // parenthesics, multiply, division, addition, sub
+
+    int demo_bocket = 6-5+4*3/2;
+
+    std::cout << "demo bocket methods:" << demo_bocket << '\n';
 
     int students = 28;
-    
+    float remainder;    
 
     //students = students + 1 ;
     //students +=1;
@@ -21,7 +26,11 @@ int main()
     //students = students /2 ;
     students /= 2;
 
+    remainder = 28%3;
+
     std::cout << students << '\n';
+    std::cout << "Remainder: " << remainder << '\n';
+
 
     return 0;
 

ddd15ab8cd44fc9f3b8e481f81803179f1f8ce2f
Vk <venkatkumar2020ece@gmail.com>
2024-01-17 22:12:14 +0530

    if-else-elseif statements completed

--- cplus/if_stat.cpp
+++ cplus/if_stat.cpp
@@ -9,17 +9,17 @@ int age;
 std::cout<< "Enter your age: ";
 std::cin>>age;
 
-if(age >= 18){
+if(age >= 18 && age<=99){
     std::cout<<"welcome to the site! enjoy the game"<< "\n";
 }
 else if(age < 0) {
-    std::cout<< "you havenot been born yet!";
+    std::cout<< "you havenot been born yet!"<< "\n";
 }
-else if {
-    std::cout<<"you are too old to enter this site!";
+else if (age >= 100){
+    std::cout<<"you are too old to enter this site!"<< "\n";
 }
 else {
-    std::cout << "you are not old enough to enter the site!";
+    std::cout << "you are not old enough to enter the site!"<< "\n";
 }
 
 return 0;

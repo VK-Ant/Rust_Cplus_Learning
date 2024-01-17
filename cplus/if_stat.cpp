3687c044acba08b19bdb08a7286add56b5a7a46e
Vk <venkatkumar2020ece@gmail.com>
2024-01-17 22:08:55 +0530

    if-else-elseif statements

---
+++ cplus/if_stat.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+int main()
+{
+
+// if statements - do something if a condition is true, if not , then don't do it
+
+int age;
+std::cout<< "Enter your age: ";
+std::cin>>age;
+
+if(age >= 18){
+    std::cout<<"welcome to the site! enjoy the game"<< "\n";
+}
+else if(age < 0) {
+    std::cout<< "you havenot been born yet!";
+}
+else if {
+    std::cout<<"you are too old to enter this site!";
+}
+else {
+    std::cout << "you are not old enough to enter the site!";
+}
+
+return 0;
+
+
+
+
+}
